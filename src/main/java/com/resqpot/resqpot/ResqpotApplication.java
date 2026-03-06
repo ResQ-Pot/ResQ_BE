71715115b5ff783af4c28aac18af46b8880a3df8
geunmoon <geunmooon@gmail.com>
2026-03-06 13:51:29 +0900

    chore: Spring Boot 초기 환경 세팅

---
+++ src/main/java/com/resqpot/resqpot/ResqpotApplication.java
@@ -0,0 +1,13 @@
+package com.resqpot.resqpot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResqpotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResqpotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/resqpot/resqpot/ResqpotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resqpot.resqpot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResqpotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
