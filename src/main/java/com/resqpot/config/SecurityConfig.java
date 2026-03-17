360ae300329c57856e8203b643f5e9ee06c84683
erun1012 <erun200@gmail.com>
2026-03-17 21:08:24 +0900

    메인 홈 대시보드 조회 테스트 추가

---
+++ src/main/java/com/resqpot/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+//Postman test 용 Spring Security 비활성화
+
+package com.resqpot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .anyRequest().permitAll()
+            );
+        return http.build();
+    }
+}
