723e24a2af3e1251aa1ab1d454319e60824b24f4
geunmoon <geunmooon@gmail.com>
2026-03-19 12:14:17 +0900

    fix: swagger 설정 수정

--- src/main/java/com/resqpot/resqpot/global/config/SwaggerConfig.java
+++ src/main/java/com/resqpot/resqpot/global/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.resqpot.resqpot.global.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server; // 이거 임포트 추가!
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,6 +12,7 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI resqpotOpenAPI() {
         return new OpenAPI()
+                .addServersItem(new Server().url("/"))
                 .info(new Info()
                         .title("ResQpot API 명세서")
                         .description("재난 안전 서비스 ResQpot의 백엔드 API 문서입니다. 안드로이드 팀 파이팅! 🚀")
